dc2ea0adcd2b0d7d41020e5ae71d9fee4f6eabef
Kio Solion <kiosolion@gmail.com>
2019-11-18 18:51:47 +0300

    Update Print func

--- vktools.py
+++ vktools.py
@@ -116,4 +116,4 @@ class vktools(object):
         names = self.GetName(ids)
         links = self.GetLink(ids)
         for i in zip(names, links):
-            print(f'{i[0]:<50} : {i[1]}')
+            print(f'{i[0]:<40} : {i[1]}')
